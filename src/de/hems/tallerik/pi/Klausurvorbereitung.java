53cc59687ada7f3b72125859190822516805fd21
Tallerik <Tallerik@users.noreply.github.com>
2021-12-01 08:42:40 +0100

    Added Klausurvo rberreitung

---
+++ src/de/hems/tallerik/pi/Klausurvorbereitung.java
@@ -0,0 +1,11 @@
+package de.hems.tallerik.pi;
+
+import java.util.LinkedList;
+
+public class Klausurvorbereitung {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Test");
+        list.addLast("Lul");
+    }
+}
