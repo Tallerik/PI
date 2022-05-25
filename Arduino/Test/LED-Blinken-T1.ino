1af78a8af82870bdd84fdd11d769ffed2e47fc5c
Tallerik <Tallerik@users.noreply.github.com>
2022-05-25 08:17:31 +0200

    LED-Blinken-T1

--- Arduino/Test/LED-Blinken-T1.ino
+++ Arduino/Test/LED-Blinken-T1.ino
@@ -1,14 +1,10 @@
 void setup() {
-    Serial.begin(9600);
     pinMode(13, OUTPUT);
 }
 
 void loop() {
-
-    Serial.write("ON");
     digitalWrite(13, HIGH);
-    delay(500);
-    Serial.write("OFF");
+    delay(1000);
     digitalWrite(13, LOW);
-    delay(500);
+    delay(1000);
 }
